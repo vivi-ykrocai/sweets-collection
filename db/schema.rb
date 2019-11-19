fd0a8b39e66bace8a2a5dabdcb094dc68d1903e2
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-11-19 18:38:59 +0900

    likeモデル及びテーブル、コントローラの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_070814) do
+ActiveRecord::Schema.define(version: 2019_11_19_092745) do
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "sweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sweet_id"], name: "index_likes_on_sweet_id"
+    t.index ["user_id", "sweet_id"], name: "index_likes_on_user_id_and_sweet_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "sweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -41,5 +51,7 @@ ActiveRecord::Schema.define(version: 2019_11_16_070814) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "sweets"
+  add_foreign_key "likes", "users"
   add_foreign_key "sweets", "users"
 end
