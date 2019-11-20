79a1fc1d6d1b2b7f81b1846ee2523243dfc6481f
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-11-20 13:12:22 +0900

    sweetsテーブルにlikes_countカラムを追加
    ・いいねの数をカウントし、ビューで表示できるようにするため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_092745) do
+ActiveRecord::Schema.define(version: 2019_11_20_035255) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_092745) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count"
     t.index ["user_id"], name: "index_sweets_on_user_id"
   end
 
