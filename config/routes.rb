5b8150fb4601dff6cfaed12ce7762cea6bf388ae
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-11-20 12:47:01 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'sweets#index'
-  resources :sweets, only: [:index, :new, :create, :show]
   resources :users, only: [:show]
+  resources :sweets, only: [:index, :new, :create, :show] do
+    resources :likes, only: [:create, :destroy]
+  end
 end
