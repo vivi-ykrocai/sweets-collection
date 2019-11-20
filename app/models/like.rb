0a7e8868e806ad7684dc36a730c879def3ae9d61
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-11-20 13:14:00 +0900

    各モデルについて、アソシエーション等の設定

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,4 @@
 class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :sweet, counter_cache: :likes_count
 end
--- app/models/sweet.rb
+++ app/models/sweet.rb
@@ -1,3 +1,5 @@
 class Sweet < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user  # 投稿がどのuserにいいねされているのかを取得
 end
