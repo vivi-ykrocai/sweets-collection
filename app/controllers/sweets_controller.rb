3e48f84efc6416ba6094a0b1dc48a3c86edf11c8
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-11-21 12:13:00 +0900

    ルーティング及びコントローラの設定
    ・sweets#searchのパスを通し、アクションを設定

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -17,6 +17,19 @@ class SweetsController < ApplicationController
     @like = Like.new
   end
 
+  def search
+    if params[:keyword1].present?
+      @search_keyword = params[:keyword1]
+      @sweets_title = Sweet.where('title LIKE(?)', "%#{params[:keyword1]}%")
+    elsif params[:keyword2].present?
+      @search_keyword = params[:keyword2]
+      @sweets_shop_name = Sweet.where('shop_name LIKE(?)', "%#{params[:keyword2]}%")
+    elsif params[:keyword3].present?
+      @search_keyword = params[:keyword3]
+      @sweets_shop_area = Sweet.where('shop_area LIKE(?)', "%#{params[:keyword3]}%")
+    end
+  end
+
   private
   def sweet_params
     params.require(:sweet).permit(:title, :image, :visit_day, :shop_name, :shop_area, :menu, :rate, :comment).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'sweets#index'
   resources :users, only: [:show]
   resources :sweets, only: [:index, :new, :create, :show] do
+    collection do
+      get 'search'
+    end
     resources :likes, only: [:create, :destroy]
   end
 end
