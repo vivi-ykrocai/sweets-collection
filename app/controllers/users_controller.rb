d314838365d05acda5c05082f4a3dcbc454e09d4
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-11-22 23:40:09 +0900

    パスの修正
    ・コメント上部に表示される名前のリンクから、各ユーザーのマイページに飛べるようパスを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(current_user.id)
-    @likes = Like.where(user_id: current_user.id)
+    @user = User.find(params[:id])
+    @likes = Like.where(user_id: @user.id)
   end
 end
