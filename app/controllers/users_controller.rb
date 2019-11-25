f46b847ba9a37ec7a0bb84b20e2723ffb6e6cd05
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-11-25 16:20:02 +0900

    修正２(ユーザー詳細ページ)
    ・レビュー詳細ページについて、投稿者のニックネームから、ユーザー詳細ページに飛べるようにパスを変更
    ・上記で飛んだ先が、自分のマイページの場合と他のユーザーの詳細ページの場合で条件分岐
    （自分のマイページの場合のみ→プロフィール編集ボタンの表示、お気に入り一覧表示）
    ・マイページで表示されるお気に入りレビューの一覧を、いいねを押した新着順で表示されるように変更
    ・ビューに崩れがあったためscssの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @likes = Like.where(user_id: @user.id)
+    @likes = Like.where(user_id: @user.id).order("id DESC")
   end
 end
